2ffcb9ce3c0b7f38b552082adb5abc0e59ff9b3f
Naren Sulegai <86naren@gmail.com>
2020-09-19 21:52:57 +0530

    Search box for events

--- Frontend/src/components/customer/Events.js
+++ Frontend/src/components/customer/Events.js
@@ -1,22 +1,31 @@
-import React, {Component} from 'react';
-import {getCustomerEvents, getEvents, registerEvent} from '../../util/fetch/api';
+import React, { Component } from 'react';
+import { getCustomerEvents, getEvents, registerEvent } from '../../util/fetch/api';
 import Event from './Event';
 
 class Events extends Component {
-
-  state = {allEvents: [], customerEvents: []}
+  constructor(props) {
+    super(props);
+    this.state = { allEvents: [], customerEvents: [] };
+    this.search = React.createRef();
+    this.handleOnRegister = this.handleOnRegister.bind(this);
+    this.handleOnSearch = this.handleOnSearch.bind(this);
+  }
 
   async componentDidMount() {
     const allEvents = await getEvents();
     const customerEvents = await getCustomerEvents();
-    this.setState({allEvents, customerEvents});
+    this.setState({ allEvents, customerEvents });
   }
 
-  handleOnRegister = async (id) => {
-    await registerEvent(id)
+  async handleOnRegister(id) {
+    await registerEvent(id);
     const allEvents = await getEvents();
     const customerEvents = await getCustomerEvents();
-    this.setState({allEvents, customerEvents});
+    this.setState({ allEvents, customerEvents });
+  }
+
+  handleOnSearch() {
+    // console.log(this.search.current.value);
   }
 
   render() {
@@ -24,13 +33,19 @@ class Events extends Component {
       <div className="row">
         <div className="col-6">
           <h4>All events</h4>
+          <div className="d-flex">
+            <input type="text" className="flex-grow-1" ref={this.search} />
+            <button className="btn-primary" onClick={this.handleOnSearch}>Search</button>
+          </div>
           {this.state.allEvents.length === 0 && <div>There are no events to show</div>}
           {this.state.allEvents.map((event) => {
             return (
-              event ? <Event event={event} key={event.id}
-                             onRegister={() => {
-                               this.handleOnRegister(event.id);
-                             }}/> : null
+              event ? (
+                <Event event={event} key={event.id}
+                  onRegister={() => {
+                    this.handleOnRegister(event.id);
+                  }} />
+              ) : null
             );
           })}
         </div>
@@ -39,12 +54,14 @@ class Events extends Component {
           {this.state.customerEvents.length === 0 && <div>You have not registered for any events</div>}
           {this.state.customerEvents.map((e, i) => {
             return (
-              e.event ? <div key={i} className="card mb-2">
-                <div className="card-header">
-                  <h4>Event <b>{e.event.name}</b></h4>
-                  <div>{e.event.description}</div>
+              e.event ? (
+                <div key={i} className="card mb-2">
+                  <div className="card-header">
+                    <h4>Event <b>{e.event.name}</b></h4>
+                    <div>{e.event.description}</div>
+                  </div>
                 </div>
-              </div> : null
+              ) : null
             );
           })}
         </div>
