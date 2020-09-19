923533dc5d68b02efc4a358a02fdb4bda1fa31f7
Naren Sulegai <86naren@gmail.com>
2020-09-19 21:27:55 +0530

    Customer event link

--- Frontend/src/components/Event.js
+++ Frontend/src/components/Event.js
@@ -12,7 +12,10 @@ const Event = ({ event, onDelete }) => {
         <h6>Hashtags {event.hashTags}</h6>
         <div>Registered users ({event.customerEvents.length})</div>
         <div>{event.customerEvents.map((c) => {
-          return <span key={c.customer.id}>{c.customer.name}&nbsp;</span>;
+          return (
+            <a href={`#/restaurant/customer/${c.customer.id}`}
+              key={c.customer.id}>{c.customer.name}</a>
+          );
         })}
         </div>
         <div>Venue at {event.location} on {event.date} at {event.time}</div>
