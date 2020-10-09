7ee5429ef47694a96b20f31729735760c0032548
Naren Sulegai <86naren@gmail.com>
2020-10-09 14:45:56 +0530

    Filter by delivery options

--- Frontend/src/components/CustomerDashboard.js
+++ Frontend/src/components/CustomerDashboard.js
@@ -1,25 +1,49 @@
-import React, { Component } from 'react';
-import { getRestaurants } from '../util/fetch/api';
+import React, {Component} from 'react';
+import {getRestaurants} from '../util/fetch/api';
 import Map from './Map';
 
 class CustomerDashboard extends Component {
   constructor(props) {
     super(props);
-    this.state = { restaurants: [] };
+    this.state = {restaurants: [], filterBy: 'all'};
   }
 
   async componentDidMount() {
     const restaurants = await getRestaurants();
-    this.setState({ restaurants });
+    this.setState({restaurants});
+  }
+
+  handleOnFilterChange = async (e) => {
+    const filterBy = e.target.value
+    const allRestaurants = await getRestaurants();
+
+    const restaurants = allRestaurants.filter(r => {
+      if (filterBy === 'all') return true;
+      if (filterBy === 'pickup') return r.isPickup;
+      if (filterBy === 'delivery') return !r.isPickup;
+      return true;
+    });
+
+    this.setState({restaurants});
   }
 
   render() {
     return (
       <div className="row">
-        <div className="col-12 mb-4">
-          <div className="d-flex">
-            <input type="text" className="flex-grow-1" />
-            <button className="btn-primary">Search</button>
+        <div className="col-12 mb-3">
+          <div className="row">
+            <div className="col-6 d-flex">
+              <input type="text" className="flex-grow-1"/>
+              <button className="btn-primary">Search</button>
+            </div>
+            <div className="col-6">
+              <span className="mr-3">Filter by delivery option</span>
+              <select defaultValue="all" onChange={this.handleOnFilterChange}>
+                <option value="all">All</option>
+                <option value="pickup">Pick up</option>
+                <option value="delivery">Yelp delivery</option>
+              </select>
+            </div>
           </div>
         </div>
         <div className="col-6">
@@ -28,9 +52,9 @@ class CustomerDashboard extends Component {
             return (
               <div key={i} className="card mb-3">
                 <div className="card-header">
-                  <h6>
+                  <h4>
                     <a href={`#/customer/restaurant/${r.id}/comments`}>{r.name}</a>
-                  </h6>
+                  </h4>
                   <div>
                     <span>Timings {r.timings || '-'}</span>
                     &nbsp;|&nbsp;
@@ -51,7 +75,7 @@ class CustomerDashboard extends Component {
         </div>
         <div className="col-6">
           {this.state.restaurants.length
-            ? <Map locations={this.state.restaurants.map((r) => r.location)} />
+            ? <Map locations={this.state.restaurants.map((r) => r.location)}/>
             : <div>No restaurants show on map</div>}
         </div>
       </div>
