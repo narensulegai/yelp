6e322cf6583bb9282441cc5df6b2b6bc8fc5abba
Naren Sulegai <86naren@gmail.com>
2020-11-01 23:19:07 +0530

    Paginate restaurant events

--- Frontend/src/components/RestaurantEvents.js
+++ Frontend/src/components/RestaurantEvents.js
@@ -1,26 +1,28 @@
-import React, { Component } from 'react';
-import { getRestaurantEvents, createEvent, deleteEvent } from '../util/fetch/api';
+import React, {Component} from 'react';
+import {getRestaurantEvents, createEvent, deleteEvent} from '../util/fetch/api';
 import Event from './Event';
 import NewEvent from './NewEvent';
+import Paginate from "./Paginate";
+import {slicePage} from "../util";
 
 class RestaurantEvents extends Component {
   constructor(props) {
     super(props);
-    this.state = { events: [] };
+    this.state = {events: [], currentPage: 0};
     this.handleOnEventAdd = this.handleOnEventAdd.bind(this);
     this.handleOnEventDelete = this.handleOnEventDelete.bind(this);
   }
 
   async componentDidMount() {
     const events = await getRestaurantEvents();
-    this.setState({ events });
+    this.setState({events});
   }
 
   async handleOnEventAdd(e) {
     createEvent(e)
       .then(async () => {
         const events = await getRestaurantEvents();
-        this.setState({ events });
+        this.setState({events});
       });
   }
 
@@ -28,26 +30,32 @@ class RestaurantEvents extends Component {
     if (window.confirm('Are you sure you want to delete this event?')) {
       await deleteEvent(id);
       const events = await getRestaurantEvents();
-      this.setState({ events });
+      this.setState({events});
     }
+  }
+
+  handleOnPageChange = (currentPage) => {
+    this.setState({currentPage})
   }
 
   render() {
     return (
       <div className="row">
         <div className="col-6">
-          <NewEvent onAdd={this.handleOnEventAdd} />
+          <NewEvent onAdd={this.handleOnEventAdd}/>
         </div>
         <div className="col-6">
           <h4>Your events</h4>
           {this.state.events.length === 0 ? <div>You have not added any events yet.</div> : null}
-          {this.state.events.map((e) => {
+          {slicePage(this.state.events, this.state.currentPage).map((e) => {
             return (
               <Event key={e.id} event={e} onDelete={() => {
                 this.handleOnEventDelete(e.id);
-              }} />
+              }}/>
             );
           })}
+          <Paginate currentPage={this.state.currentPage} numItems={this.state.events.length}
+                    onPageChange={this.handleOnPageChange}/>
         </div>
       </div>
     );
