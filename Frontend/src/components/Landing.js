0ce4ceb4e8e6b32e05c96c7b35ff122aa1f2864c
Naren Sulegai <86naren@gmail.com>
2020-11-03 11:30:39 +0530

    Refactor

--- Frontend/src/components/Landing.js
+++ Frontend/src/components/Landing.js
@@ -1,45 +1,45 @@
-import React, { PureComponent } from 'react';
-import { withRouter, NavLink } from 'react-router-dom';
-import { get } from '../util/fetch';
+import React, {PureComponent} from 'react';
+import {withRouter, NavLink} from 'react-router-dom';
 import LoginPage from './LoginPage';
+import {currentUser} from "../util/fetch/api";
 
 export class Landing extends PureComponent {
   constructor(props) {
     super(props);
-    this.state = { currTab: 'customer' };
-    this.handleOnLogin = this.handleOnLogin.bind(this);
-    this.toggleLogin = this.toggleLogin.bind(this);
+    this.state = {currTab: 'customer'};
   }
 
   async componentDidMount() {
-    const currUser = await get('currentUser');
+    const currUser = await currentUser();
+    //Redirect logged in users automatically
     if (currUser.scope !== null) {
       this.props.history.push(currUser.scope === 'customer' ? '/customer/dashboard' : '/restaurant/profile');
     }
   }
 
-  handleOnLogin() {
+  handleOnLogin = async () => {
+    //Redirect user after login
     this.props.history.push(this.state.currTab === 'customer' ? '/customer/dashboard' : '/restaurant/profile');
   }
 
-  toggleLogin() {
+  toggleLogin = () => {
     if (this.state.currTab === 'customer') {
-      this.setState({ currTab: 'restaurant' });
+      this.setState({currTab: 'restaurant'});
     } else {
-      this.setState({ currTab: 'customer' });
+      this.setState({currTab: 'customer'});
     }
   }
 
   render() {
-    const { currTab } = this.state;
+    const {currTab} = this.state;
     return (
       <div className="container">
         <div className="row">
-          <div className="col-3" />
+          <div className="col-3"/>
           <div className="col-6">
             <div className="h3 text-center mt-3">Yelp!</div>
             <div className="text-center">{`Sign in as a ${currTab}`}</div>
-            <LoginPage onLogin={this.handleOnLogin} type={currTab} />
+            <LoginPage onLogin={this.handleOnLogin} type={currTab}/>
 
             <div className="d-flex justify-content-center">
               <div>Dont have a account ?&nbsp;&nbsp;</div>
@@ -55,7 +55,7 @@ export class Landing extends PureComponent {
               </button>
             </div>
           </div>
-          <div className="col-3" />
+          <div className="col-3"/>
         </div>
       </div>
     );
