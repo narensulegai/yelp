174125f3d8d70666e35c312bded66e7295855b63
Naren Sulegai <86naren@gmail.com>
2020-11-03 19:40:19 +0530

    1. Paginate dish orders 2. Fixing issue with order type

--- Frontend/src/components/restaurant/PlacerOrder.js
+++ Frontend/src/components/restaurant/PlacerOrder.js
@@ -3,10 +3,13 @@ import PropTypes from 'prop-types';
 import { getRestaurantDishes, placeOrder } from '../../util/fetch/api';
 import Carousal from '../Carousal';
 import AddReview from './AddReview';
+import Paginate from '../Paginate';
+import { slicePage } from '../../util';
 
 const PlaceOrder = ({ restaurantId }) => {
   const [dishes, setDishes] = useState([]);
-  const deliveryMode = useRef();
+  const [currentPage, setCurrentPage] = useState(0);
+  const deliveryMode = useRef({});
 
   useEffect(() => {
     (async () => {
@@ -16,34 +19,39 @@ const PlaceOrder = ({ restaurantId }) => {
 
   const handlePlaceOrder = async (dishId) => {
     // TODO
-    await placeOrder(dishId, { isPickup: deliveryMode.current.value === 'pickup' });
+    await placeOrder(dishId, { isPickup: deliveryMode.current[dishId].value === 'pickup' });
     window.alert('Your order has been placed!');
   };
 
   return (
     <div className="mb-3">
       {dishes.length === 0 ? <div>Restaurant has not listed any dishes yet.</div> : null}
-      {dishes.map((d) => {
+      {slicePage(dishes, currentPage).map((d) => {
         return (
           <div className="card mb-3" key={d.id}>
             <div className="card-header d-flex">
               {/* Order dish */}
               <div className="flex-grow-1">
+
                 <div><b>{d.name}</b></div>
                 <div className="small">{d.description}</div>
                 <div className="small">Made with {d.ingredients}</div>
+
                 <div className="mt-2">
-                   <Carousal images={d.fileIds} />
+                  <Carousal images={d.fileIds} />
                 </div>
+
                 <div className="mt-2">Order one for <b>${d.price}</b></div>
+
                 <div>
                   <label className="mr-3">Select delivery method</label>
-                  <select defaultValue="delivery" ref={deliveryMode} className="mr-3">
+                  <select defaultValue="delivery" ref={(e) => deliveryMode.current[d.id] = e} className="mr-3">
                     <option value="delivery">Yelp delivery</option>
                     <option value="pickup">Pickup</option>
                   </select>
                   <button className="btn-primary" onClick={() => handlePlaceOrder(d.id)}>Order dish</button>
                 </div>
+
               </div>
               {/* Review dish */}
               <div className="flex-grow-1">
@@ -53,6 +61,7 @@ const PlaceOrder = ({ restaurantId }) => {
           </div>
         );
       })}
+      <Paginate numItems={dishes.length} onPageChange={setCurrentPage} currentPage={currentPage} />
     </div>
   );
 };
